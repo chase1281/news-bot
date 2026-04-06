414dad42e9e0a289276f07186fae54579ca1c37a
kimminje <kmz34180@naver.com>
2026-04-06 14:46:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/example/newsBot/NewsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
